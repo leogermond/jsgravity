c5c3186ef9dad192b388c0aabb049c87cb615c70
Léo Germond <leo.germond@gmail.com>
2017-09-19 20:36:15 +0200

    wip realistic weights and distances

--- equations.js
+++ equations.js
@@ -2,8 +2,24 @@
 
 function Body(p, s, a, m) {
 	this.psa = math.matrix([ p, s, a ]);
-	this.m = m;
+    this.m = m;
 	
+    this.getPSA = function(dim) {
+        return this.psa.toArray()[dim];
+    };
+
+    this.getP = function() {
+        return this.getPSA(0);
+    };
+
+    this.getS = function() {
+        return this.getPSA(1);
+    };
+
+    this.getA = function() {
+        return this.getPSA(2);
+    };
+
 	this.tick = function(f, t) {
         if(f === undefined) {
 			f = [0, 0];
@@ -54,11 +70,10 @@ function Universe(bodies, G) {
         for(var i = 0; i < nb_bodies; i++) {    
             for(var j = i + 1; j < nb_bodies; j++) {
                 var b = [this.bodies[i], this.bodies[j]];
-                var sub = math.subtract(math.multiply([1, 0, 0], b[1].psa),
-                                        math.multiply([1, 0, 0], b[0].psa));
+                var sub = math.subtract(b[1].getP(), b[0].getP());
                 var d = math.norm(sub);
                 // handle collision
-                if(d != 0) {
+                if(d != 0 && 1e10 * d < b[0].m * b[1].m) {
                     var gfij = math.multiply(this.G * (b[0].m * b[1].m) / (d*d*d), sub);
                     gf[i] = math.add(gf[i], gfij);
                     gf[j] = math.add(gf[j], math.unaryMinus(gfij));
@@ -78,12 +93,15 @@ function Universe(bodies, G) {
         var nb_bodies = this.bodies.length;
         var islow = 1;
         for(var i = 0; i < nb_bodies; i++) {
-            if(math.norm(gf[i]) > this.bodies[i].m*1000) {
-                islow = math.norm(gf[i])/(this.bodies[i].m*1000);
-            }
+            var b = this.bodies[i];
+            var v = math.max(math.norm(b.getS()), 1e3);
+            islow = math.max(islow, math.floor(math.pow(t * 1e4 * math.norm(gf[i]) / (b.m * v), 1.2)));
         }
 
-        console.log(islow);
+        islow = math.min(1e6, islow);
+        if(islow != 1) {
+            console.log(islow);
+        }
 
         for(var j = 0; j < islow; j++) {
             var tslow = t / islow;
